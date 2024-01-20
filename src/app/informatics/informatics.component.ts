96944c25e477caefb72a398782f63b6405886a2c
Mankunian <dias.07.04@gmail.com>
2024-01-20 23:08:44 +0600

    client side

--- src/app/informatics/informatics.component.ts
+++ src/app/informatics/informatics.component.ts
@@ -9,11 +9,11 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
   templateUrl: './informatics.component.html',
   styleUrls: ['./informatics.component.scss']
 })
-export class InformaticsComponent implements OnInit{
+export class InformaticsComponent implements OnInit {
   parentData!: ParentDatum[];
   childData!: ParentDatum[];
   selectedSubTopic: any;
-  selectedParentIndex!: number;
+  routeTopicIndex!: number;
   routeSubtopicId!: number;
 
   constructor(
@@ -25,8 +25,7 @@ export class InformaticsComponent implements OnInit{
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
-      this.selectedParentIndex = +params['topicId']; // 2
-      console.log(this.selectedParentIndex);
+      this.routeTopicIndex = +params['topicId']; // 2
       this.routeSubtopicId = +params['subtopicId'];
     });
 
@@ -36,8 +35,8 @@ export class InformaticsComponent implements OnInit{
   getParentTopics() {
     this.apiService.getParentTopics().subscribe(response => {
       this.parentData = response;
-      if (this.selectedParentIndex) { // 2
-        const parentId = response[this.selectedParentIndex - 1]?.id;
+      if (this.routeTopicIndex) { // 2
+        const parentId = response[this.routeTopicIndex - 1]?.id;
         console.log(parentId) // 8
         this.getTopicByParentId(parentId);
       }
@@ -52,30 +51,26 @@ export class InformaticsComponent implements OnInit{
     this.apiService.getTopicByParentId(parentId).subscribe(children => {
       this.childData = children;
       if (this.routeSubtopicId) {
-        const filteredChild = this.childData.filter(child=> child?.id == this.routeSubtopicId);
+        const filteredChild = this.childData.filter(child => child?.id == this.routeSubtopicId);
         this.selectedSubTopic = filteredChild[0];
         console.log(this.selectedSubTopic)
       }
-      // this.selectedSubTopic = children[this.selectedParentIndex];
     })
   }
 
   ifRouteHasTopic(parentIndex: number) {
-    const topicId = this.selectedParentIndex; // 1
+    const topicId = this.routeTopicIndex; // 1
     return !!(topicId && (topicId - 1) == parentIndex);
   }
 
   isRouteHasSubtopic(index: number, children: ParentDatum) {
-    // console.log(this.selectedSubTopic) // element, object;
-    // const filtered = this.childData.filter(child => child.id == this.selectedSubTopic?.id);
-    // console.log(filtered)
-  return false;
+    return this.selectedSubTopic?.id === children.id;
   }
 
   onClickChild(children: any, parentIndex: number) {
     const parentTopic = parentIndex + 1;
     this.router.navigate(['/informatics/topic/' + parentTopic + '/subtopic/', children.id])
-    this.selectedParentIndex = parentIndex;
+    this.routeTopicIndex = parentIndex;
     this.selectedSubTopic = children;
   }
 
