9fc10157a54377219547f1dd581174c6a47ac6bd
Галихан Хамитов <galikhan.khamitov@zee.kz>
2024-01-31 12:06:23 +0600

    fix design add login support

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  password = '';
+  username = '';
+  constructor(public authService: AuthService, public router: Router) { }
+
+  ngOnInit(): void {
+
+
+  }
+
+  login(): void {
+    this.authService.login(this.username, this.password).subscribe(result => {
+      if(result) {
+        this.router.navigate(['/admin']);
+      }
+    });
+  }
 }
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://161.97.144.45:8282/api/v1'
+  apiUrl: 'http://161.97.144.45:8282/api/v1',
+  apiPrivateUrl: 'http://161.97.144.45:8282/api/v1/private',
+  domain: 'http://161.97.144.45'
 };
