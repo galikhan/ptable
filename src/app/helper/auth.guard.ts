e7383a668602257a76e04904b79c1190b9ad6496
Mankunian <dias.07.04@gmail.com>
2024-02-02 23:31:47 +0600

    add features, bugFix

---
+++ src/app/helper/auth.guard.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from "../service/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.isAuthenticated()) {
+      // If the user is authenticated, allow access to the route
+      return true;
+    } else {
+      // If the user is not authenticated, redirect to the login page
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+  }
+
+}
