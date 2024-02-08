932452b5ce0a2b1921495897e8409a9d2a810c8c
Mankunian <dias.07.04@gmail.com>
2024-02-08 20:44:04 +0600

    1. install angular youtube

---
+++ src/app/shared/video-player/video-player.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss']
+})
+export class VideoPlayerComponent implements OnInit {
+  private apiLoaded = false;
+
+  @Input() videoId!: string;
+
+  ngOnInit(): void {
+    console.log('videoId', this.videoId)
+    if (!this.apiLoaded) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
+  }
+}
