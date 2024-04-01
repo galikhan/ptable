b610bdee4d9bbb47ffd8562ad1784e5edd38d0fb
Галихан Хамитов <galikhan.khamitov@zee.kz>
2024-04-01 05:09:45 +0500

    update api https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://s-art.kz/api/v1',
-  apiPrivateUrl: 'http://s-art.kz/api/v1/private',
-  domain: 'http://s-art.kz',
+  apiUrl: 'https://s-art.kz/api/v1',
+  apiPrivateUrl: 'https://s-art.kz/api/v1/private',
+  domain: 'https://s-art.kz',
 };
